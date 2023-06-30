22c5ccd2171727340f4543fd4ef546c4d6ca0c61
himanshu kumar <hk7703940916@gmail.com>
2023-06-30 17:08:48 +0530

    Added new migration to add new coloumns

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -5,7 +5,8 @@ const {ValidationError , AppError} = require('../utils/errors/index');
 class BookingRepository{
     async create(data){
         try {
-            
+            const booking = await Booking.create(data);
+            return booking;
         } catch (error) {
             if(error.name == 'SequelizeValidationError'){
                throw new ValidationError(error);
